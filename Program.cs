e472978eb808f120a06e4db5bc375d87f5497dc2
advx9600 <nwz88348307@163.com>
2015-06-01 14:21:36 +0800

    version 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateGenDbSqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Domain.TbApps).Assembly);
+
+            // Get ourselves an NHibernate Session
+            var sessions = cfg.BuildSessionFactory();
+            var sess = sessions.OpenSession();
+
+            var tbConfig = new Domain.TbApps
+            {
+                name = "abcd"
+            };
+            // And save it to the database
+            sess.Save(tbConfig);
+            sess.Flush();
+        }
+    }
+}
